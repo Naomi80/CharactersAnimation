423081ac9f581a065af830088dc332088c273490
Ixales <alexis.ardourel2003@gmail.com>
2021-11-22 14:37:29 +0100

    1

--- Tableau1.js
+++ Tableau1.js
@@ -1,12 +1,33 @@
 class Tableau1 extends Phaser.Scene{
-    preload(){
-        for(let id=1;id<=10;id++){
-            this.load.image('idle-2'+id,'Characters/boy/boy_4/PNG/idle2'+id+'.png');
+    preload() {
+        for (let id = 1; id <= 10; id++) {
+            this.load.image('layer' + id, 'Characters/boy/boy_4/PNG/idle2/Layer' + id + '.png');
         }
-        create(){
-            {Phaser.GameObjects.TileSprite}
-            let idle2=this.add.tileSprite(0,0,'idle2').setOrigin(0,1)
-        }
+    }
+    create(){
+            /**
+             * AnimationIdle2
+             * @type  {Phaser.GameObjects.TileSprite}
+             */
+            this.idle2=this.add.sprite(0,0,'layer').setOrigin(0,0)
+            this.anims.create({
+                key: 'idle2',
+                frames: [
+                    {key:'layer-1'},
+                    {key:'layer-2'},
+                    {key:'layer-3'},
+                    {key:'layer-4'},
+                    {key:'layer-5'},
+                    {key:'layer-6'},
+                    {key:'layer-7'},
+                    {key:'layer-8'},
+                    {key:'layer-9'},
+                    {key:'layer-10'},
+                ],
+                frameRate: 16,
+                repeat: -1
+            });
+            this.layer.play('idle2');
 
     }
 }
